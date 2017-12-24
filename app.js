3b69cde915fad3705aeffbecc9bbf82fb86fe3a1
Elusha <34803882+elusha123@users.noreply.github.com>
2017-12-24 13:00:33 +0800

    upload

---
+++ app.js
@@ -0,0 +1,65 @@
+<script type="text/javascript">
+  function HELLOWORLD() {
+    alert("HELLOWORLD")
+  }
+
+  a = 1;
+  b = 3;
+  c = a + b;
+  console.log(c);
+  a = 100;
+  c = a + b
+  console.log(c);
+
+  a = 1;
+  a = a + 1;
+  console.log(a);
+
+  aaa = 1;
+  bbb = "YO";
+  console.log(aaa+1);
+  console.log(bbb+1);
+
+  age = 17
+  if(age >= 18){
+    console.log("我可以喝酒");
+  }
+  else {
+    console.log("我還不能喝酒");
+  }
+
+  age = 20
+  if (age >= 20) {
+    console.log("我可以投票");
+  }
+  else{
+    console.log("我還沒有投票權");
+  }
+
+  a = [5,6,0,9,8];
+  console.log(a);
+  console.log(a[2]);
+  console.log("a序列=" + a);
+
+  b = 1
+  for ( b = 1; b <= 5; b = b + 1){
+    console.log(b);
+  }
+
+  function abc(x){
+    x = (x+10)*2/3;
+    return x;
+  }
+  result = abc(2);
+  console.log(result);
+
+  function asd(y){
+    z = (y+y)*3/4;
+    return y*z;
+  }
+  o = asd(1)
+  console.log(o);
+
+
+
+</script>
